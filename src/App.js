bf1cdaf565b7f1e5183b0dc9e4168bc9558d75c4
Bradley Tian <bradley_tian@berkeley.edu>
2023-04-07 15:54:55 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
+
+
+import { useState, useEffect } from 'react';
+import { Button, Input } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import './App.css';
+import title from "./PlexGPT.png";
+import PlexieRegular from "./PRegular.png";
+import PlexieThinking from "./PThinking.png";
+import PlexieResponding from "./PRespond.png";
 
 function App() {
+
+  const theme = createTheme({
+    status: {
+      danger: "#e53e3e",
+    },
+    palette: {
+      primary: {
+        main: "#ff8a00",
+      },
+      neutral: {
+        main: "#ffffff",
+        contrastText: "#ff8a00",
+      },
+    },
+  });
+
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+  const [plexie, setPlexie] = useState(PlexieRegular);
+
+  async function makeRequest() {
+    let URL = `${process.env.REACT_APP_BACKEND_URL}text_completion/${prompt}`
+    console.log(URL);
+    setPlexie(PlexieThinking);
+    await fetch(URL, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(data => {
+        setPlexie(PlexieResponding)
+        let trimmed = data.answer.content.charAt(0) === '?' ? data.answer.content.slice(3) : data.answer.content
+        setResponse(trimmed)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='Page'>
+        <ThemeProvider theme={theme}>
+          <div style={{ display: 'block', margin: '5%', }}>
+            <img src={title} style={{ width: '30%', marginBottom: "2rem"}} />
+            <span style={{ display: 'flex', marginBottom: '50px' }}>
+              <img src={plexie} style={{ width: '20%', marginRight: "5rem", }} />
+              <p style={{ minHeight: '10%' }} className="Response" >{response}</p>
+            </span>
+            <span className="Input">
+              <Input value={prompt} style={{ width: "28%" }} onChange={(event) => setPrompt(event.target.value)}></Input>
+              <Button
+                variant="contained"
+                color="neutral"
+                fontWeight="Bold"
+                onClick={makeRequest}
+                style={{ "marginLeft": "1rem" }}
+              >Ask Plexie</Button>
+              <Button
+                variant="contained"
+                color="neutral"
+                fontWeight="Bold"
+                onClick={() => {
+                  setPrompt("");
+                  setResponse("");
+                  setPlexie(PlexieRegular);
+                }}
+                style={{ "marginLeft": "1rem" }}
+              >Clear</Button>
+            </span>
+          </div>
+        </ThemeProvider>
+      </div>
+    </>
   );
 }
 
