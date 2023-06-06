2fecb0b288808a27b6ca6aa6f8ef65223c39f9f1
Bradley Tian <bradley_tian@berkeley.edu>
2023-06-06 04:45:01 -0400

    flipped logical operand

--- src/pages/sourcing.js
+++ src/pages/sourcing.js
@@ -34,6 +34,7 @@ function SourcingTool() {
     const [response, setResponse] = useState([]);
     const errorMsg = "I'm sorry,  I wasn't able to find any contact information about this person.";
     const navigate = useNavigate();
+    let searched = false;
 
     async function submitInfo() {
         if (processing) {
@@ -63,11 +64,13 @@ function SourcingTool() {
                     setPlexie(PlexieError);
                     setResponse(errorMsg);
                     setProcessing(false);
+                    searched = true;
                 }
                 else {
                     setResponse(data);
                     setProcessing(false);
                     setPlexie(PlexieResponding)
+                    searched = true;
                 }
 
             })
@@ -75,6 +78,7 @@ function SourcingTool() {
                 setPlexie(PlexieError);
                 setResponse([errorMsg]);
                 setProcessing(false);
+                searched = true;
             })
     }
 
@@ -115,7 +119,7 @@ function SourcingTool() {
                                     {processing ? <Typography variant="p">Please wait...</Typography> : <></>}
                                 </div>
                                 {response[0] === errorMsg || response.length === 0 ? <></> : <Typography variant="h6">Here are some possibly valid email addresses:<br /></Typography>}
-                                {response.length === 0 ? <Typography variant="p">I'm sorry, I couldn't find any email addresses for this person.</Typography> : response.map((email) => <Typography variant="p">{email}<br /></Typography>)}
+                                {response.length !== 0 ? response.map((email) => <Typography variant="p">{email}<br /></Typography>) : <></>}
                             </div>
                         </span>
                     </div>
